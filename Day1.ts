ae882bb91f43bb5c938202138e1b9ca7835a9e7b
khuloodmaqbool <101542490+khuloodmaqbool@users.noreply.github.com>
2024-03-11 05:43:39 -0700

    Add files via upload

---
+++ Day1.ts
@@ -0,0 +1,11 @@
+ // Question 2
+ let personName1:string ="Khulood";
+ let message:string = `Hello ${personName1}, would you like to learn some Python today?`;
+ console.log(message);
+ 
+           // Question 3
+ let personName2:string = "KHULOOD"
+ console.log(personName2.toUpperCase());
+ console.log(personName2.toLowerCase());
+ var titleCase:string = personName2[0].toUpperCase() + personName2.slice(1).toLowerCase()
+ console.log(titleCase);
